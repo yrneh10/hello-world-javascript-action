7e4d4002ac007dc56d6e2c812d5a7a3190f26b21
yrneh10 <83069210+yrneh10@users.noreply.github.com>
2025-07-18 15:43:18 +0800

    Update index2.js

--- index2.js
+++ index2.js
@@ -11,6 +11,7 @@ try {
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
     const pattoken = "dsadwqiddsa1231"
+    const password = "samep23ikew"
 } catch (error) {
     core.setFailed(error.message);
 }
